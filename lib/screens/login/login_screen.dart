898c5ad7acf7b9936c5b3f8d88f8dd50f85cface
Chala Alemayehu <chalaalemayehu1824@gmail.com>
2024-11-01 20:24:41 +0300

    Completing Login page UI

--- lib/screens/login/login_screen.dart
+++ lib/screens/login/login_screen.dart
@@ -1,10 +1,12 @@
 import 'package:flutter/material.dart';
+import 'package:recipe_pp/widgets/form_widgets.dart';
 
 class LoginPage extends StatelessWidget {
   const LoginPage({super.key});
 
   @override
   Widget build(BuildContext context) {
+    final formState = GlobalKey<FormState>();
     return Scaffold(
       backgroundColor: Color(0xFFEEEEEC),
       body: Center(
@@ -16,17 +18,89 @@ class LoginPage extends StatelessWidget {
             borderRadius: BorderRadius.circular(10.0),
           ),
           child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
             children: [
-              Text(
-                'Log in',
-                style: TextStyle(
-                  fontSize: 20.0,
+              const Padding(
+                padding: EdgeInsets.symmetric(vertical: 5.0, horizontal: 15.0),
+                child: Text(
+                  'Log in',
+                  style: TextStyle(
+                    fontSize: 30.0,
+                    fontWeight: FontWeight.bold,
+                  ),
                 ),
               ),
-              SizedBox(
+              const SizedBox(
                 height: 5.0,
               ),
-              Form(child: ,),
+              Form(
+                key: formState,
+                child: Column(
+                  children: [
+                    FormWidgets(formTitle: "Email", hintText: "Enter email"),
+                    // Reusable widget
+                    const SizedBox(
+                      height: 30.0,
+                    ),
+                    FormWidgets(
+                        formTitle: "Password", hintText: "Enter password"),
+                    const SizedBox(
+                      height: 30.0,
+                    ),
+                    // Reusable widget
+                    ElevatedButton(
+                      onPressed: () {},
+                      style: ElevatedButton.styleFrom(
+                        backgroundColor: Colors.green,
+                        padding: const EdgeInsets.symmetric(
+                            vertical: 20.0, horizontal: 100.0),
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(5.0),
+                        ),
+                      ),
+                      child: const Text(
+                        "Submit",
+                        style: TextStyle(
+                          color: Colors.white70,
+                        ),
+                      ),
+                    ),
+                    const SizedBox(
+                      height: 15.0,
+                    ),
+                    Center(
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          InkWell(
+                            onTap: () {},
+                            // TODO 1: Handle when the user TAPS the forgot password link clicked.
+                            child: const Text(
+                              "Forgot Email?",
+                              style: TextStyle(
+                                color: Colors.blue,
+                              ),
+                            ),
+                          ),
+                          const SizedBox(
+                            width: 20.0,
+                          ),
+                          InkWell(
+                            onTap: () {},
+                            // TODO 2: Handle the user TAPS when the forgot password link clicked.
+                            child: const Text(
+                              "Forgot password?",
+                              style: TextStyle(
+                                color: Colors.blue,
+                              ),
+                            ),
+                          )
+                        ],
+                      ),
+                    )
+                  ],
+                ),
+              ),
             ],
           ),
         ),
--- lib/widgets/form_widgets.dart
+++ lib/widgets/form_widgets.dart
@@ -1,10 +1,31 @@
 import 'package:flutter/material.dart';
 
 class FormWidgets extends StatelessWidget {
-  const FormWidgets({super.key});
+  final String formTitle; // Prop. String
+  final String hintText; // Prop. String
+  FormWidgets({required this.formTitle, required this.hintText});
 
   @override
   Widget build(BuildContext context) {
-    return ;
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 15.0),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Text(
+            formTitle,
+          ),
+          const SizedBox(
+            height: 5.0,
+          ),
+          TextFormField(
+            decoration: InputDecoration(
+              hintText: hintText,
+              border: OutlineInputBorder(),
+            ),
+          ),
+        ],
+      ),
+    );
   }
 }
